f488d191c86b9a2044add8f0e47b51e2e0b87a2c
alexsharm <alfex.sharm@gmail.com>
2024-05-28 11:40:52 +0300

    done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
-import contactsRouter from "./routes/contactsRouter.js";
+import router from "./routes/router.js";
 
 import "./db.js";
 
@@ -14,7 +14,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api", router);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
